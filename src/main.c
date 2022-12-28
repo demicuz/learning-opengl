55382ad55d375424d65185b991020865b30e919d
Denis Mazurak <pure.demicube@gmail.com>
2022-12-29 00:35:08 +0300

    Draw a quad using an EBO!

--- src/main.c
+++ src/main.c
@@ -6,6 +6,20 @@
 
 #include <trying-into-opengl.h>
 
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0"; // TODO aren't they null-terminated by default?
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0"; // TODO aren't they null-terminated by default?
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
@@ -22,9 +36,10 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__ // TODO do I even need this?
+
+#ifdef __APPLE__ // TODO do I even need this?
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
+#endif
 
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -61,6 +76,18 @@ int main(void) {
          0.0F,  0.5F, 0.0F
     };
 
+    float quad_vertices[] = {
+         0.5F,  0.5F, 0.0F,  // top right
+         0.5F, -0.5F, 0.0F,  // bottom right
+        -0.5F, -0.5F, 0.0F,  // bottom left
+        -0.5F,  0.5F, 0.0F   // top left
+    };
+
+    GLuint indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+    };
+
     GLuint VAO;
     GLuint VBO;
     glGenVertexArrays(1, &VAO);
@@ -69,25 +96,18 @@ int main(void) {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_vertices), quad_vertices, GL_STATIC_DRAW);
 
-    const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0"; // TODO aren't they null-terminated by default?
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\0"; // TODO aren't they null-terminated by default?
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -98,9 +118,9 @@ int main(void) {
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    // TODO should I also do this in the render loop?
-    glUseProgram(shaderProgram);
-    // glBindVertexArray(VAO);
+    // TODO does this do anything?
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     // TODO where do I put these?
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
@@ -108,9 +128,9 @@ int main(void) {
     glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(posAttrib);
 
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    // TODO should I also do this in the render loop?
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -121,7 +141,8 @@ int main(void) {
         // Render
         // glUseProgram(shaderProgram);
         // glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
