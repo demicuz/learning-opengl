6b122f7c832afb01c85a4b34bcf59bc9ff3c8f6b
Denis Mazurak <pure.demicube@gmail.com>
2023-01-05 01:40:31 +0300

    Use `#define` instead of `const` in headers

--- include/trying-into-opengl.h
+++ include/trying-into-opengl.h
@@ -1,7 +1,11 @@
 #ifndef TRYING_INTO_OPENGL_H
 #define TRYING_INTO_OPENGL_H
 
-const int WIDTH = 800, HEIGHT = 600;
-const float ASPECT = (float) WIDTH / (float) HEIGHT;
+// const int WIDTH = 800, HEIGHT = 600;
+// const float ASPECT = (float) WIDTH / (float) HEIGHT;
+
+#define WIDTH 800
+#define HEIGHT 600
+#define ASPECT (float) WIDTH / (float) HEIGHT
 
 #endif
